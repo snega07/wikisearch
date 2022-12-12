b42645f4a55cfa2b9b69e4af6e1319065761ae6b
snega07 <snegapalanisamy@gmail.com>
2022-12-12 22:48:32 +0530

    wikisearch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [result, setResult] = useState({});
+
+  async function fetchData(val) {
+    const searchtext = val.trim().toLowerCase();
+    console.log(searchtext);
+    if (searchtext.length > 0) {
+      const apistring = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchtext}`;
+        const response = await fetch(apistring);
+        if (!response.ok) {
+            throw Error(response.statusText)
+        }
+        return await response.json();
+    }
+}
+async function handleSearch(e) {
+  const { value } = e.target;
+  setResult({ result: await fetchData(value) })
+  console.log(result)
+}
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input type="text" className="search" onKeyUp={handleSearch} />
+   
+    <ul>
+    {result.result && result.result.query.search.map((data,i) =>{return(<div key={i}>{data.title}</div>)})}
+ </ul>
     </div>
   );
 }
